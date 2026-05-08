b123b12a0b80f620a7eda5529c675e565d86ef71
Flix <nsulagodu@gmail.com>
2026-05-08 09:45:10 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,92 +1,73 @@
 #include <iostream>
-#include <vector>
 #include <string>
 #include <cmath>
 using namespace std;
 
-double avg(const vector<double>& vals) {
-    if (vals.empty()) return 0;
+double avg(double vals[], int size) {
+    if (size == 0) return 0;
 
     double sum = 0;
-    for (double v : vals) sum += v;
+    for (int i = 0; i < size; i++) sum += vals[i];
 
-    return round((sum / vals.size()) * 10) / 10.0;
+    return round((sum / size) * 10) / 10.0;
 }
 
 int main() {
-    vector<int> nums = {1,2,3,4,5,6,7,8,9,10};
+    int nums[] = {1,2,3,4,5,6,7,8,9,10};
+    int numsSize = sizeof(nums) / sizeof(nums[0]);
 
-    for (int n : nums) cout << n << " ";
+    for (int i = 0; i < numsSize; i++) cout << nums[i] << " ";
     cout << endl;
 
-    for (int i = (int)nums.size() - 1; i >= 0; i--) cout << nums[i] << " ";
+    for (int i = numsSize - 1; i >= 0; i--) cout << nums[i] << " ";
     cout << endl;
 
-
-
-
-    
     int evenCount = 0;
-    for (int n : nums) if (n % 2 == 0) evenCount++;
+    for (int i = 0; i < numsSize; i++) if (nums[i] % 2 == 0) evenCount++;
     cout << evenCount << endl;
 
+    double sales[5];
+    string days[5] = {"MondDay","Tuesday","Wednesday","Thursday","friday"};
+    int salesSize = sizeof(sales) / sizeof(sales[0]);
 
-
-    
-    vector<double> sales(5);
-    vector<string> days = {"MondDay","Tuesday","Wednesday","Thursday","friday"};
-
-    for (int i = 0; i < (int)sales.size(); i++) {
+    for (int i = 0; i < salesSize; i++) {
         cout << "Enter sales for " << days[i] << ": ";
         cin >> sales[i];
     }
 
-
-    
     string cont = "yes";
     while (cont == "yes" || cont == "Yes") {
         int day;
 
-        
-        cout << "Pick a day (1-" << sales.size() << "): ";
+        cout << "Pick a day (1-" << salesSize << "): ";
         cin >> day;
 
-        
-
-        if (day >= 1 && day <= (int)sales.size()) {
+        if (day >= 1 && day <= salesSize) {
             cout << sales[day - 1] << endl;
-
-            
         } else {
-
-            
             cout << "INvalid day" << endl;
-            
         }
 
         cout << "Continue????? ";
         cin >> cont;
     }
 
-    
-    vector<int> ages(20);
-    for (int i = 0; i < (int)ages.size(); i++) {
+    int ages[20];
+    int agesSize = sizeof(ages) / sizeof(ages[0]);
+
+    for (int i = 0; i < agesSize; i++) {
         cout << "Enter age # " << i + 1 << ": ";
         cin >> ages[i];
     }
 
-
-
-
-
-    
     int countAbove15 = 0;
-    for (int a : ages) if (a > 15) countAbove15++;
+    for (int i = 0; i < agesSize; i++) if (ages[i] > 15) countAbove15++;
     cout << countAbove15 << endl;
 
-    vector<double> grades = {85.8, 93.7, 76, 88.5, 100, 91.3};
+    double grades[] = {85.8, 93.7, 76, 88.5, 100, 91.3};
+    int gradesSize = sizeof(grades) / sizeof(grades[0]);
 
-    cout << avg(grades) << endl;
+    cout << avg(grades, gradesSize) << endl;
 
     return 0;
 }
